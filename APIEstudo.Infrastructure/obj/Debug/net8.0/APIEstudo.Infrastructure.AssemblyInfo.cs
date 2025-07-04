23bc2a7000d4713ec8c87286b149d502f39d4751
lucas <lucaspatriciodasilvalj@gmail.com>
2025-07-04 17:18:15 -0300

    commit

--- APIEstudo.Infrastructure/obj/Debug/net8.0/APIEstudo.Infrastructure.AssemblyInfo.cs
+++ APIEstudo.Infrastructure/obj/Debug/net8.0/APIEstudo.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("APIEstudo.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8419ce09e726ab8350aca66953fbf36cf74ca82d")]
 [assembly: System.Reflection.AssemblyProductAttribute("APIEstudo.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("APIEstudo.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
